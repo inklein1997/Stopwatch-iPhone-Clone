7e2d4786b0f83f39253d65686eb78391e72df1b7
Michael Klein <Michaelklein1997@gmail.com>
2021-12-30 00:03:37 -0600

    start button starts timer and changes to stop button which will stop timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,44 @@ const rightButtonEl = document.querySelector('[data-start]');
 const recordsEl = document.querySelector('[data-laprecords]');
 const lapRecordEl1 = document.createElement('div');
 
+var timeInterval
+
 var displayMilliseconds
 var displayMilliseconds
 var displayMinutes
 var displayHours
 
-rightButtonEl.addEventListener('click', function() {
-    startTimer();
+var millisecondsTime
+var secondsTime
+var minutesTime
+var hoursTime
+
+rightButtonEl.addEventListener('click', function(event) {
+    var element = event.target
+
+    if(element.matches("button")) {
+        var state = element.getAttribute("data-state");
+        console.log(state)
+        if (state === "Start") {
+            element.setAttribute("data-state","data-stop");
+            element.textContent = "Stop";
+            element.setAttribute("style","background-color:#320E0B; color:#FF453A;box-shadow:0 0 0 0.5vw #320E0B");
+            startTimer()
+        } else {
+            element.setAttribute("data-state","Start");
+            element.setAttribute("style","background-color:#082A11; color:#2ED158;box-shadow:0 0 0 0.5vw #082A11")
+            element.textContent = "Start";
+            clearTimeout(timeInterval)
+        }
+    }
 })
 
-function startTimer() {
-    var millisecondsTime = 0
-    var secondsTime = 55
-    var minutesTime = 59
-    var hoursTime = 0
-    
-    var timeInterval = setInterval(function() {
+function startTimer() { 
+    millisecondsTime = 0
+    secondsTime = 0
+    minutesTime = 0
+    hoursTime = 0
+    timeInterval = setInterval(function() {
         millisecondsTime = millisecondsTime + 1
         if (millisecondsTime >= 100) {
             secondsTime++;
