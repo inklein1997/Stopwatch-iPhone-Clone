ca4f97e7b132c9dd888936f00ec3c8e4d194b33c
Michael Klein <Michaelklein1997@gmail.com>
2021-12-29 23:03:30 -0600

    fix timer to have consistent layout when value is less than 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,10 @@ const rightButtonEl = document.querySelector('[data-start]');
 const recordsEl = document.querySelector('[data-laprecords]');
 const lapRecordEl1 = document.createElement('div');
 
-var time = 0
+var displayMilliseconds
+var displayMilliseconds
+var displayMinutes
+var displayHours
 
 rightButtonEl.addEventListener('click', function() {
     startTimer();
@@ -18,14 +21,33 @@ function startTimer() {
     var timeInterval = setInterval(function() {
         millisecondsTime = millisecondsTime + 1
         if (millisecondsTime >= 100) {
-            secondsTime = secondsTime + 1;
+            secondsTime++;
             millisecondsTime = 0;
+
+            if (secondsTime >= 60) {
+                minutesTime++;
+                secondsTime = 0;
         }
-        if (secondsTime >= 60) {
-            minutesTime = minutesTime + 1;
-            secondsTime = 0;
-        }
-        timerEl.textContent = minutesTime + ":" + secondsTime + "." + millisecondsTime
+    }
+
+    if(millisecondsTime <= 9) {
+        displayMilliseconds = "0" + millisecondsTime.toString();
+    } else {
+        displayMilliseconds = millisecondsTime
+    }
+    if(secondsTime <= 9) {
+        displaySeconds = "0" + secondsTime.toString();
+    } else {
+        displaySeconds = secondsTime
+    }
+    if(minutesTime <= 9) {
+        displayMinutes = "0" + minutesTime.toString();
+    } else {
+        displayMinutes = minutesTime
+    }
+    timerEl.textContent = displayMinutes+":"+displaySeconds+"."+displayMilliseconds
+
+        
     },10)
 } 
 
